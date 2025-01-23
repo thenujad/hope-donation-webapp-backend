daa7aa51531289453c8dc8c579c14790a383ef96
thenujad <shihanrandina@gmail.com>
2025-01-23 13:20:57 +0530

    initialized microservices

---
+++ file-upload/src/test/java/com/hope/file_upload/FileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hope.file_upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ request-management/src/main/java/com/hope/request_management/RequestManagementApplication.java
@@ -0,0 +1,13 @@
+package com.hope.request_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestManagementApplication.class, args);
+	}
+
+}
