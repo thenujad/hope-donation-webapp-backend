fa67a9359822bd77736039282d0a19d2fb633f27
thenujad <shihanrandina@gmail.com>
2025-01-24 17:24:25 +0530

    Config service completed

--- config-service/src/main/java/com/hope/config_service/ConfigServiceApplication.java
+++ config-service/src/main/java/com/hope/config_service/ConfigServiceApplication.java
@@ -2,6 +2,7 @@ package com.hope.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
