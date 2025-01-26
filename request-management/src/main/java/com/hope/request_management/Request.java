7802897d31c2e3d78d80a39947c5320ee9c815e5
thenujad <shihanrandina@gmail.com>
2025-01-26 23:07:22 +0530

    request updated

--- request-management/src/main/java/com/hope/request_management/Request.java
+++ request-management/src/main/java/com/hope/request_management/Request.java
@@ -1,7 +1,9 @@
 package com.hope.request_management;
 
+import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Data
 @Document(collection = "requests")
 public class Request {
 
@@ -9,38 +11,4 @@ public class Request {
     private String userId;
     private String description;
     private String status;
-
-    // Getters and Setters
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
