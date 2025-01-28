aa32ad8b6757e57c45440fed10c732848624301b
thenujad <shihanrandina@gmail.com>
2025-01-28 15:40:09 +0530

    errors fixed

--- request-management/src/main/java/com/hope/request_management/Request.java
+++ request-management/src/main/java/com/hope/request_management/Request.java
@@ -2,23 +2,23 @@ package com.hope.request_management;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+@Data
 @Builder
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 @Document(collection = "request")
 public class Request {
     @JsonSerialize(using = ToStringSerializer.class)
     @MongoId(FieldType.OBJECT_ID)
     private String id;
+
     private String userId;
     private String description;
     private String status;
--- request-management/src/main/java/com/hope/request_management/RequestController.java
+++ request-management/src/main/java/com/hope/request_management/RequestController.java
@@ -1,6 +1,5 @@
 package com.hope.request_management;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -30,5 +29,4 @@ public class RequestController {
         return ResponseEntity.ok(requestService.updateRequestStatus(id, status));
     }
 }
-
 
